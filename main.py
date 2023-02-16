f9f289390807ee6cebcbd660a6986d644b1293a9
AndrewB <avborovets@gmail.com>
2023-02-16 17:14:11 +0300

    all can be drawn

--- main.py
+++ main.py
@@ -1,89 +1,132 @@
 
 from tkinter import *
-from decimal import *
 
-root = Tk()
-root.title('Calculator')
+from model import Field
 
-buttons = (('7', '8', '9', '/', '4'),
-           ('4', '5', '6', '*', '4'),
-           ('1', '2', '3', '-', '4'),
-           ('0', '.', '=', '+', '4')
-           )
+# root = Tk()
+# root.title('Calculator')
 
-activeStr = ''
-stack = []
+# buttons = (('7', '8', '9', '/', '4'),
+#            ('4', '5', '6', '*', '4'),
+#            ('1', '2', '3', '-', '4'),
+#            ('0', '.', '=', '+', '4')
+#            )
 
-
-def calculate():
-    global stack
-    global label
-    result = 0
-    operand2 = Decimal(stack.pop())
-    operation = stack.pop()
-    operand1 = Decimal(stack.pop())
-
-    if operation == '+':
-        result = operand1 + operand2
-    if operation == '-':
-        result = operand1 - operand2
-    if operation == '/':
-        result = operand1 / operand2
-    if operation == '*':
-        result = operand1 * operand2
-    label.configure(text=str(result))
+# activeStr = ''
+# stack = []
 
 
+# def calculate():
+#     global stack
+#     global label
+#     result = 0
+#     operand2 = Decimal(stack.pop())
+#     operation = stack.pop()
+#     operand1 = Decimal(stack.pop())
 
-def click(text):
-    global activeStr
-    global stack
-    if text == 'CE':
-        stack.clear()
-        activeStr = ''
-        label.configure(text='0')
-    elif '0' <= text <= '9':
-        activeStr += text
-        label.configure(text=activeStr)
-    elif text == '.':
-        if activeStr.find('.') == -1:
-            activeStr += text
-            label.configure(text=activeStr)
-    else:
-        if len(stack) >= 2:
-            stack.append(label['text'])
-            calculate()
-            stack.clear()
-            stack.append(label['text'])
-            activeStr = ''
-            if text != '=':
-                stack.append(text)
-        else:
-            if text != '=':
-                stack.append(label['text'])
-                stack.append(text)
-                activeStr = ''
-                label.configure(text='0')
+#     if operation == '+':
+#         result = operand1 + operand2
+#     if operation == '-':
+#         result = operand1 - operand2
+#     if operation == '/':
+#         result = operand1 / operand2
+#     if operation == '*':
+#         result = operand1 * operand2
+#     label.configure(text=str(result))
 
 
 
-label = Label(root, text='0', width=35)
-label.grid(row=0, column=0, columnspan=4, sticky="nsew")
+# def click(text):
+#     global activeStr
+#     global stack
+#     if text == 'CE':
+#         stack.clear()
+#         activeStr = ''
+#         label.configure(text='0')
+#     elif '0' <= text <= '9':
+#         activeStr += text
+#         label.configure(text=activeStr)
+#     elif text == '.':
+#         if activeStr.find('.') == -1:
+#             activeStr += text
+#             label.configure(text=activeStr)
+#     else:
+#         if len(stack) >= 2:
+#             stack.append(label['text'])
+#             calculate()
+#             stack.clear()
+#             stack.append(label['text'])
+#             activeStr = ''
+#             if text != '=':
+#                 stack.append(text)
+#         else:
+#             if text != '=':
+#                 stack.append(label['text'])
+#                 stack.append(text)
+#                 activeStr = ''
+#                 label.configure(text='0')
 
-button = Button(root, text='CE', command=lambda text='CE': click(text))
-button.grid(row=1, column=3, sticky="nsew")
-for row in range(4):
-    for col in range(4):
-        button = Button(root, text=buttons[row][col],
-                command=lambda row=row, col=col: click(buttons[row][col]))
-        button.grid(row=row + 2, column=col, sticky="nsew")
-
-root.grid_rowconfigure(6, weight=1)
-root.grid_columnconfigure(4, weight=1)
-
-root.mainloop()
 
 
+# label = Label(root, text='0', width=35)
+# label.grid(row=0, column=0, columnspan=4, sticky="nsew")
 
-# if __name__ == '__main__':
-#     play_desktop()
+# button = Button(root, text='CE', command=lambda text='CE': click(text))
+# button.grid(row=1, column=3, sticky="nsew")
+# for row in range(4):
+#     for col in range(4):
+#         button = Button(root, text=buttons[row][col],
+#                 command=lambda row=row, col=col: click(buttons[row][col]))
+#         button.grid(row=row + 2, column=col, sticky="nsew")
+
+# root.grid_rowconfigure(6, weight=1)
+# root.grid_columnconfigure(4, weight=1)
+
+# root.mainloop()
+
+def get_text(player):
+    if player == 1:
+        return 'X'
+    elif player == -1:
+        return 'O'
+
+
+def click(root, field, row, col):
+    field.make_step((row, col))
+    print(field)
+    draw(root, field)
+    
+
+def draw(root, field):
+    for row in range(field.sizes):
+        for col in range(field.sizes):
+            button = Button(root, text=get_text(field[row][col]),
+                    command=lambda root=root, field=field, row=row, col=col: click(root, field, row, col))
+            button.grid(row=row + 2, column=col, sticky="nsew")
+
+
+
+def play_desktop(sizes=3):
+
+    field = Field(3)
+
+    root = Tk()
+    root.title('tic-tac-toe')
+
+    button = Button(root, text='start again', command=lambda coordinates: click(coordinates))
+    button.grid(row=2, column=3, sticky="nsew")
+
+    draw(root, field)
+
+    root.grid_rowconfigure(6, weight=1)
+    root.grid_columnconfigure(4, weight=1)
+
+    root.mainloop()
+
+
+
+
+    
+
+if __name__ == '__main__':
+    play_desktop(sizes=3)
--- model.py
+++ model.py
@@ -1,36 +1,37 @@
 from enum import Enum
 import numpy as np
 
-class Player(Enum):
-    tic = 1
-    toc = -1
+# class Player(Enum):
+#     tic = 1
+#     toc = -1
+#     nobody = 0
 
-class BadStep:
+class BadStep(Exception):
     pass
 
 
 class Field:
     def __init__(self, sizes=3):
         self.sizes = sizes
-        self.field = np.zeros((sizes, sizes))
-        self.player = Player.tic
+        self.field = np.zeros((sizes, sizes), dtype=int)
+        self.player = 1 #  Player.tic
     
     def check_win(self, player):
         for i in range(self.sizes):
             if sum(self.field[i, :]) * self.player == 3:
-                return True
+                return True, [(i, 0), (i, self.sizes)]
             if sum(self.field[:, i]) * self.player == 3:
-                return True
+                return True, [(0, i), (self.sizes, i)]
         sum_left = 0
         sum_right = 0
         for i in range(self.sizes):
             sum_left += self.field[i, i]
             sum_right += self.field[self.sizes - i - 1, self.sizes - i - 1]
         if sum_left * player == 3:
-            return True
+            return True, [(0, 0), (self.sizes, self.sizes)]
         if sum_right * player == 3:
-            return True
-        return False
+            return True, [(0, self.sizes), (self.sizes, 0)]
+        return False, []
     
     def draw(self):
         pass
@@ -39,7 +40,7 @@ class Field:
         y, x = step
         if self.field[y][x] != 0:
             raise BadStep()
-        self.field[y][x] = self.step
+        self.field[y][x] = self.player
         
         is_win, where = self.check_win(self.player)
 
@@ -47,5 +48,11 @@ class Field:
             self.draw()
         
         self.player *= -1
+    
+    def __getitem__(self, key):
+        return self.field[key]
+
+    def __repr__(self):
+        return str(self.field)
 
         
